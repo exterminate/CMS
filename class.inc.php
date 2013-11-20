164f061bdcab193f7f965df697914dd5543c967f
Ian <ian@Ian-Coatess-iMac.local>
2013-11-20 16:40:48 +0000

    initial build

--- class.inc.php
+++ class.inc.php
@@ -1,6 +1,36 @@
 <?php
 
 	// class.inc.php
+	
+	class MyDB extends SQLite3 
+	{
+		function __construct() {
+	        $this->open('myDatabase.db');
+		}
+	}
+	$db = new MyDB();
+	$db->exec('CREATE TABLE if not exists user 
+	    (ID TEXT PRIMARY KEY NOT NULL,
+	    time TEXT NOT NULL,
+	    name TEXT NOT NULL,
+	    psswrd TEXT NOT NULL,
+	    )');
+	
+	class Setup
+	{
+		public function __construct($db) {
+
+		}	
+	}
+	
+	class Page 
+	{
+		public function __construct($site,$title) {
+			$this->site = $site;
+			$this->title = $title;
+		}	
+		
+	}
 
 	class User 
 	{
--- index.php
+++ index.php
@@ -4,10 +4,15 @@
 	*	CMS created by Ian Coates for http://exterminate.me
 	*	It is free to use or fork.
 	*	Please give me a shout out if you plan to use it on your site.
-	*	
+	*	Get it at https://github.com/exterminate/CMS
 	*/
 	
     // index.php
+
 	require('class.inc.php');
+	
+	
+	
+	$page = new Page($site,$title);
 	
 ?>
